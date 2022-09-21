a9fc4c642bc1d5374551f4cb5ae0963cd8b2c3dc
Luiz Guilherme <luizguilhermecb99@gmail.com>
2022-09-20 22:27:41 -0300

    Ex44

--- EstruturaDeRepeticao/Ex44.py
+++ EstruturaDeRepeticao/Ex44.py
@@ -1,18 +1,25 @@
 from ast import While, match_case
 
+votacao = 0
 
-voto = int(input("Digite seu voto \n [1]Manoel \t [2]Elaine \t [3]Jorge \t [4]Creusa \t [5]Nulo \t [6]Voto em Branco"))
-votacao = input()
+while votacao == 0:
 
-M = voto['1']
-E = voto['2'] 
-J = voto['3'] 
-C = voto['4'] 
-N = voto['5'] 
-B = voto['6'] 
+    voto = int(input("Digite seu voto \n [1]Manoel \t [2]Elaine \t [3]Jorge \t [4]Creusa \t [5]Nulo \t [6]Voto em Branco \n"))
+    votacao = input("Digite [0] para começar e [7] para terminar")
+    
+    M = voto[1]
+    E = voto[2] 
+    J = voto[3] 
+    C = voto[4] 
+    N = voto[5] 
+    B = voto[6] 
 
+    T = M + E + J + C + N + B # Voto total
 
-While (votacao == 's')
+    PN = N/T # Porcentagem de votos nulos/total
+
+    PB = B/T # Porcentagem de votos brancos/total
+
     match voto:
 
         case [1]:
@@ -33,17 +40,21 @@ While (votacao == 's')
         case [6] :
             print("O Número de Votos em Voto em Branco é igual a : ", B)
             B+=B
-    votacao=input("Deseja continuar votando ? \t [s]sim \t [n]não")
-    break
+         
+
+
+    
+print("Fim de Votação \n")
     
 
-
-
-print("Resultado da Votação")
+print("Resultado da Votação \n")
 print("Votos em Manoel = ",M)
 print("Votos em Elaine = ",E)
 print("Votos em Jorge = ",J)
 print("Votos em Creusa = ",C)
+print("----------------------")
 print("Votos Nulo = ",N)
 print("Votos em Branco = ",B)
+print("porcentagem votos nulos/total = ",PN)
+print("porcentagem votos brancos/total = ",PB)
 
